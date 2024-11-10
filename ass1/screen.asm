bb9d375e0435aabb039b95940b4158b4350e093d
lukaexmedicinec <kalsekluka@gmail.com>
2024-11-10 23:47:46 +0100

    spremenjen screen.asm

--- ass1/screen.asm
+++ ass1/screen.asm
@@ -9,7 +9,6 @@ screen	START 0
 	JSUB	scrfill
 	JSUB	scrclear
 	JSUB	printch
-	JSUB	printstr
 
 
 halt	J	halt
@@ -46,47 +45,6 @@ printch
 
 	RSUB
 
-printstr
-	LDA	row
-	MUL	scrcols
-	ADD	col
-	ADD	scree
-	SUB	#1
-	STA	targt
-	LDX	#0
-
-inp					.branje napisanega
-		. da ne popackamo registrov
-	TD	stdin
-	JEQ	inp
-	RD	stdin		. beremo vhod
-	LDB	#10
-	COMPR 	A, B		. preverimo ce je newline na koncu
-	JEQ	inp_end		. ce je greo na konec
-	LDB	#0
-	COMPR	A, B
-	JEQ	halt
-	STCH	strng, X	. shranimo char na index X v A
-	TIX	#0		. inkrementiramo x ampak nic ne preverjamo
-	J	inp
-
-inp_end	LDA	#0		. na X damo null terminator
-	STCH	strng, X	. na ix X je null terminator
-	LDA	temp		. ponastavimo A
-
-	LDB	#1
-	SUBR	B,X
-	SUBR	B,X
-	LDA	scrend	
-	SUB	targt	
-pisi	
-	LDCH	strng, X 	. v A nalozimo char
-	RMO	A,B		. zdaj je v B
-    	SUB	 #1 . target inkrementiramo
-    	STA 	targt
-    	+STB 	@targt . napisemo znak na target .
-    	COMP 	scree
-    	JGT 	pisi
 
 
 scree	WORD	X'00B800'
@@ -99,9 +57,4 @@ col	WORD	40			.v sredini izpise
 row	WORD	12
 stdin	WORD	0
 
-strng	RESB	255		. max stevilo crk v vrstici
-adresa	RESW	1
-temp	RESW	1
-buff	RESB	10
-ostnk	RESW	1
 	END screen
